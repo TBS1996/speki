81eb22da4e0d8784640c5479a7e65a7175514eeb
Tor <torberge@outlook.com>
2022-07-14 21:50:19 +0200

    merge krossterm w main

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,97 @@
-pub mod krossterm;
 pub mod ui;
 pub mod logic;
 pub mod events;
 pub mod utils;
-
-mod app;
+pub mod app;
 
 
 use std::env;
+use crate::{app::App};
+use crate::utils::sql::init_db;
+use crossterm::{
+    event::{self, DisableMouseCapture, 
+        EnableMouseCapture, Event},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, 
+        EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{
+    error::Error,
+    io,
+    time::{Duration, Instant},
+};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    Terminal,
+};
 
-use crate::krossterm::run;
-use std::error::Error;
+
+
 
 
 fn main() -> Result<(), Box<dyn Error>> {
     env::set_var("RUST_BACKTRACE", "1");
-    run()?;
+    init_db().expect("Failed to create sqlite database");
+
+
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // create app and run it
+    let app = App::new();
+    let res = run_app(&mut terminal, app, Duration::new(0, 25));
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {println!("{:?}", err)}
+
     Ok(())
+}
+
+
+
+
+
+
+fn run_app<B: Backend>(
+    terminal: &mut Terminal<B>,
+    mut app: App,
+    tick_rate: Duration,
+) -> io::Result<()> 
+{
+    let mut last_tick = Instant::now();
+    loop {
+        terminal.draw(|f| ui::draw(f, &mut app))?;
+
+        let timeout = tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+        if crossterm::event::poll(timeout)? {
+            if let Event::Key(key) = event::read()? {
+                app.handle_key(key.code);
+            }
+        }
+
+        if last_tick.elapsed() >= tick_rate {
+            app.on_tick();
+            last_tick = Instant::now();
+        }
+
+        if app.should_quit {
+            return Ok(());
+        }
+
+    }
 }

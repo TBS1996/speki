e837518d3dd1dc5c693f22d05abdc75192a12fd7
tor <torberge@outlook.com>
2022-10-25 21:01:04 +0200

    squashing test

--- src/widgets/cardlist.rs
+++ src/widgets/cardlist.rs
@@ -1,13 +1,26 @@
 use std::fmt;
 use std::fmt::Display;
+use std::sync::{Arc, Mutex};
+
+use rusqlite::Connection;
 
 use crate::utils::aliases::*;
+use crate::utils::sql::fetch::fetch_question;
 use crate::utils::statelist::KeyHandler;
 
 #[derive(Debug, Clone)]
 pub struct CardItem {
     pub question: String,
     pub id: CardID,
+}
+
+impl CardItem {
+    pub fn from_id(conn: &Arc<Mutex<Connection>>, id: CardID) -> Self{
+        Self {
+            question: fetch_question(conn, id),
+            id,
+        }
+    }
 }
 
 impl KeyHandler for CardItem {}
